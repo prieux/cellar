9f4910de281644a29a0da1357ea6921a93a74ceb
Pascal Rieux <pascalrieux64@gmail.com>
2018-03-20 10:14:34 +0000

    the password file is now a parameter

--- get-password.sh
+++ get-password.sh
@@ -5,12 +5,24 @@
 # to encrypt the whole file:
 #  openssl aes-256-cbc -salt -in private -out private.enc
 
+PASSFILE=$1
+if [ "$PASSFILE" == "" ] || [ ! -f $PASSFILE ]
+then
+    echo "No passfile provided, exiting..."
+    exit 1
+fi
 read -sp "Password: " OPENSSLPASS
+export SERVICE=""
 echo ""
-read -sp "Service: " SERVICE
-SERVICE="$(echo "$SERVICE"|tr -d '\n')"
-OPENSSLPASS=$OPENSSLPASS openssl aes-256-cbc -salt -d -in private.enc -pass env:OPENSSLPASS | \
-    awk '/'"$SERVICE"'/ {do_print=1} do_print==1 {print} NF==0 {do_print=0}' | \
-    less
+read -srp "service:" SERVICE
+while [ "$SERVICE" != "" ]
+do
+    SERVICE="$(echo "$SERVICE"|/usr/bin/tr -d '\n')"
+    OPENSSLPASS=$OPENSSLPASS openssl aes-256-cbc -salt -d -in $PASSFILE -pass env:OPENSSLPASS | \
+        /usr/bin/awk '/'"$SERVICE"'/ {do_print=1} do_print==1 {print} NF==0 {do_print=0}' | \
+        /usr/bin/less
+    echo ""
+    read -srp "service:" SERVICE
+done
 unset SERVICE
 unset OPENSSLPASS
